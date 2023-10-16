25ec6e43d854c0e9aeb19786e70c6ad938b815d4
Recyclingbottle <hoholeo382@gmail.com>
2023-10-16 15:27:47 +0900

    14. 프로젝트 모집 게시글

---
+++ backend/routes/project_board.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+const Post = require('../models/post');
+const { SECRET_KEY } = require('./jwt_config');
+
+// JWT 토큰 인증 미들웨어
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.status(403).json({ error: '토큰이 필요합니다.' });
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) return res.status(403).json({ error: '토큰이 유효하지 않습니다.' });
+
+        req.user = decoded;
+        next();
+    });
+};
+
+// 전체 게시물 조회 (조회수 오르지 않음)
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.findAll();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({ error: '게시물을 불러오는 중 오류가 발생했습니다.' });
+    }
+});
+
+// 개별 게시물 조회 (조회수 1 증가)
+router.get('/:postId', async (req, res) => {
+    const postId = req.params.postId;
+    try {
+        const post = await Post.findByPk(postId);
+        if (!post) return res.status(404).json({ error: '게시물을 찾을 수 없습니다.' });
+
+        // 조회수 1 증가
+        post.view_count++;
+        await post.save();
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json({ error: '게시물을 불러오는 중 오류가 발생했습니다.' });
+    }
+});
+
+// 게시물 등록
+router.post('/', authenticateToken, async (req, res) => {
+    try {
+        const postData = {
+            ...req.body,
+            user_id: req.user.id
+        };
+        const post = await Post.create(postData);
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(500).json({ error: '게시물 등록에 실패했습니다.' });
+    }
+});
+
+// 게시물 수정
+router.put('/:postId', authenticateToken, async (req, res) => {
+    const postId = req.params.postId;
+    try {
+        const result = await Post.update(req.body, { where: { post_id: postId, user_id: req.user.id } });
+        if (result[0] === 0) return res.status(404).json({ error: '수정할 게시물을 찾을 수 없습니다.' });
+        res.status(200).json({ message: '게시물이 성공적으로 수정되었습니다.' });
+    } catch (error) {
+        res.status(500).json({ error: '게시물 수정에 실패했습니다.' });
+    }
+});
+
+// 게시물 삭제
+router.delete('/:postId', authenticateToken, async (req, res) => {
+    const postId = req.params.postId;
+    try {
+        const result = await Post.destroy({ where: { post_id: postId, user_id: req.user.id } });
+        if (result === 0) return res.status(404).json({ error: '삭제할 게시물을 찾을 수 없습니다.' });
+        res.status(200).json({ message: '게시물이 성공적으로 삭제되었습니다.' });
+    } catch (error) {
+        res.status(500).json({ error: '게시물 삭제에 실패했습니다.' });
+    }
+});
+
+// 좋아요 기능
+router.post('/:postId/like', authenticateToken, async (req, res) => {
+    const postId = req.params.postId;
+    try {
+        const post = await Post.findByPk(postId);
+        if (!post) return res.status(404).json({ error: '게시물을 찾을 수 없습니다.' });
+
+        // 좋아요 1 증가
+        post.likes++;
+        await post.save();
+
+        res.status(200).json({ message: '게시물에 좋아요를 성공적으로 눌렀습니다.' });
+    } catch (error) {
+        res.status(500).json({ error: '좋아요 기능에 오류가 발생했습니다.' });
+    }
+});
+
+module.exports = router;
