6a9e18950933b57d7e4fba64644fd4bac96e1719
Recyclingbottle <hoholeo382@gmail.com>
2023-10-17 16:40:14 +0900

    메인페이지 수정

--- frontend/src/components/MainPageComponent.js
+++ frontend/src/components/MainPageComponent.js
@@ -1,10 +1,41 @@
-import React from 'react';
-//메인 페이지
+import React, { useState, useEffect } from 'react';
+import HeaderComponent from './HeaderComponent';
+import Navbar from './Navbar';
 function MainPageComponent() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/pboard');
+                const data = await response.json();
+                setPosts(data);
+            } catch (error) {
+                console.error("게시글 로딩에 실패했습니다", error);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
     return (
         <div>
-            <h2>메인화면입니다</h2>
+            <Navbar />
+            <HeaderComponent />
+            <div className="main-content">
+                <h2>옅구름</h2>
+                <p>토이프로젝트 팀원 찾기 사이트</p>
+                <input type="text" placeholder="검색어를 입력하세요" />
 
+                <div className="cards">
+                    {posts.map(post => (
+                        <div key={post.post_id} className="card">
+                            <h3>{post.project_title}</h3>
+                            <p>{post.post_content}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
