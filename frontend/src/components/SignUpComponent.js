0372d383b52009ff0d7d44f2083850f1e76a9969
Recyclingbottle <hoholeo382@gmail.com>
2023-10-17 16:40:32 +0900

    회원가입 코드 업그레이드

--- frontend/src/components/SignUpComponent.js
+++ frontend/src/components/SignUpComponent.js
@@ -1,27 +1,45 @@
-// SignUpComponent.js
-
 import React, { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar';
+import HeaderComponent from './HeaderComponent';
 function SignUpComponent() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [fullName, setFullName] = useState('');
-    const [nickname, setNickname] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+        fullName: '',
+        nickname: '',
+        phoneNumber1: '',
+        phoneNumber2: '',
+        phoneNumber3: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({ ...prevState, [name]: value }));
+    };
 
     const handleSignUp = async (e) => {
         e.preventDefault();
 
-        // 회원가입 요청을 보낼 데이터 객체 생성
+        const { password, confirmPassword, phoneNumber1, phoneNumber2, phoneNumber3 } = formData;
+
+        if (password !== confirmPassword) {
+            alert('비밀번호가 일치하지 않습니다.');
+            return;
+        }
+
+        const phoneNumber = `${phoneNumber1}-${phoneNumber2}-${phoneNumber3}`;
         const userData = {
-            email,
+            email: formData.email,
             password,
-            full_name: fullName,
-            nickname,
-            phone_number: phoneNumber,
+            full_name: formData.fullName,
+            nickname: formData.nickname,
+            phone_number: phoneNumber
         };
 
-        // 서버에 회원가입 요청
         const response = await fetch('http://localhost:5000/auth/register', {
             method: 'POST',
             headers: {
@@ -31,62 +49,43 @@ function SignUpComponent() {
         });
 
         if (response.ok) {
-            // 회원가입 성공 처리
-            console.log('회원가입 성공');
+            alert('회원가입 성공');
+            navigate('/login'); //로그인 페이지로 이동시켜줍니다
         } else {
-            // 회원가입 실패 처리
-            console.error('회원가입 실패');
+            alert('회원가입 실패');
         }
     };
 
     return (
         <div>
-            <h2>Sign Up</h2>
+            <Navbar />
+            <HeaderComponent />
+            <h2>회원가입</h2>
             <form onSubmit={handleSignUp}>
+                {[
+                    { label: '이메일', type: 'text', name: 'email' },
+                    { label: '비밀번호', type: 'password', name: 'password' },
+                    { label: '비밀번호 확인', type: 'password', name: 'confirmPassword' },
+                    { label: '성명', type: 'text', name: 'fullName' },
+                    { label: '닉네임', type: 'text', name: 'nickname' },
+                ].map((field, idx) => (
+                    <div key={idx}>
+                        <label>{field.label}:</label>
+                        <input
+                            type={field.type}
+                            name={field.name}
+                            value={formData[field.name]}
+                            onChange={handleChange}
+                        />
+                    </div>
+                ))}
                 <div>
-                    <label>이메일:</label>
-                    <input
-                        type="text"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>비밀번호:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>성명:</label>
-                    <input
-                        type="text"
-                        name="full_name"
-                        value={fullName}
-                        onChange={(e) => setFullName(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>닉네임:</label>
-                    <input
-                        type="text"
-                        name="nickname"
-                        value={nickname}
-                        onChange={(e) => setNickname(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Phone Number:</label>
-                    <input
-                        type="text"
-                        name="phone_number"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
+                    <label>전화번호:</label>
+                    <input type="text" maxLength="3" name="phoneNumber1" value={formData.phoneNumber1} onChange={handleChange} />
+                    -
+                    <input type="text" maxLength="4" name="phoneNumber2" value={formData.phoneNumber2} onChange={handleChange} />
+                    -
+                    <input type="text" maxLength="4" name="phoneNumber3" value={formData.phoneNumber3} onChange={handleChange} />
                 </div>
                 <div>
                     <button type="submit">Sign Up</button>
