b9a52b7efa454f58afd27c9d5fddbb37ac200279
Recyclingbottle <hoholeo382@gmail.com>
2023-10-17 16:37:50 +0900

    네비게이션 바 컴포넌트 생성

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../actions/authActions';
+import '../styles/NavbarStyle.css'; // NavbarStyle.css 파일을 가져옴
+
+function Navbar() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate('/login');
+    };
+
+    return (
+        <div className="navbar"> {/* 스타일이 적용될 클래스 이름 추가 */}
+            <Link to="/">홈</Link>
+            <Link to="/profile">프로필</Link>
+            <Link to="/notifications">알림</Link>
+            {isAuthenticated ? (
+                <button onClick={handleLogout}>로그아웃</button>
+            ) : (
+                <Link to="/login">로그인</Link>
+            )}
+        </div>
+    );
+}
+
+export default Navbar;
