f6179b033c8f198550113fc398170748e32ce3bf
Recyclingbottle <hoholeo382@gmail.com>
2023-10-17 16:40:43 +0900

    필요한 컴포넌트 루트 추가

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,6 +6,8 @@ import { useSelector } from 'react-redux';
 import LoginComponent from './LoginComponent';
 import SignUpComponent from './SignUpComponent';
 import MainPageComponent from './MainPageComponent';
+import Profile from './profile';
+import RegisterProfile from './register_profile';
 
 function App() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
@@ -15,6 +17,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginComponent />} />
         <Route path="/signup" element={<SignUpComponent />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register_profile" element={<RegisterProfile />} />
         <Route path="/" element={isAuthenticated ? <MainPageComponent /> : <LoginComponent />} />
       </Routes>
     </Router>
