7cc3ce1ae7658d2845eb41ff97809d42626042e7
Recyclingbottle <hoholeo382@gmail.com>
2023-10-17 16:37:10 +0900

    BE - 프로필이 존재하지 않으면 메세지 전달으로 변경

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -23,12 +23,15 @@ const authenticateToken = (req, res, next) => {
 router.get('/', authenticateToken, async (req, res) => {
     try {
         const profile = await ProfileInfo.findOne({ where: { user_id: req.user.id } });
-        if (!profile) return res.status(404).json({ error: '프로필을 찾을 수 없습니다.' });
+        if (!profile) {
+            return res.status(200).json({ message: '프로필이 존재하지 않습니다. 등록하세요.' });
+        }
         res.status(200).json(profile);
     } catch (error) {
         res.status(500).json({ error: '프로필 정보를 가져오는 중 오류가 발생하였습니다.' });
     }
 });
+
 
 router.post('/register', authenticateToken, async (req, res) => {
     try {
@@ -53,48 +56,5 @@ router.put('/update', authenticateToken, async (req, res) => {
     }
 });
 
-//그냥 전체 수정하는 로직으로 사용하고 
-//스킬셋, 경험, 포트폴리오 각각 하나만 수정하는 로직을 삭제해도 좋을 거 같다
-// router.put('/skills', authenticateToken, async (req, res) => {
-//     try {
-//         const updatedProfile = await ProfileInfo.update({ skill_set: req.body.skills }, { where: { user_id: req.user.id } });
-
-//         if (updatedProfile[0] === 0) {
-//             return res.status(404).json({ error: '프로필을 찾을 수 없습니다.' });
-//         }
-
-//         res.status(200).json({ message: '스킬셋이 성공적으로 등록되었습니다.' });
-//     } catch (error) {
-//         res.status(500).json({ message: '스킬셋 등록에 실패하였습니다.' });
-//     }
-// });
-
-// router.put('/experience', authenticateToken, async (req, res) => {
-//     try {
-//         const updatedProfile = await ProfileInfo.update({ experience: req.body.experience }, { where: { user_id: req.user.id } });
-
-//         if (updatedProfile[0] === 0) {
-//             return res.status(404).json({ error: '프로필을 찾을 수 없습니다.' });
-//         }
-
-//         res.status(200).json({ message: '경력이 성공적으로 등록되었습니다.' });
-//     } catch (error) {
-//         res.status(500).json({ message: '경력 등록에 실패하였습니다.' });
-//     }
-// });
-
-// router.put('/portfolio', authenticateToken, async (req, res) => {
-//     try {
-//         const updatedProfile = await ProfileInfo.update({ portfolio_link: req.body.link }, { where: { user_id: req.user.id } });
-
-//         if (updatedProfile[0] === 0) {
-//             return res.status(404).json({ error: '프로필을 찾을 수 없습니다.' });
-//         }
-
-//         res.status(200).json({ message: '포트폴리오 링크가 성공적으로 수정되었습니다.' });
-//     } catch (error) {
-//         res.status(500).json({ error: '포트폴리오 링크 수정에 실패하였습니다.' });
-//     }
-// });
 
 module.exports = router;
