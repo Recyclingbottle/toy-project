bb0fd8c6a3a5df8c82543d776f91666f282ae366
Recyclingbottle <hoholeo382@gmail.com>
2023-10-20 09:06:03 +0900

    게시글 편집, 알림 컴포넌트 Path 설정

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,18 @@
 // App.js
 //2023-10-18
+//로그인, 회원가입
+//프로필 등록, 수정
+//게시글 등록, 수정, 삭제
+//참가 요청 - 수락 및 거절 
+//Q&A 등록 삭제 수정
+//알림 페이지 - 참가요청 알람, 수락 혹은 거절 알람, Q&A 알람
+
 //post id 하나의 게시글 자세히 보는 페이지 , 수정, 삭제  + Q & A 
-//Q&A 등록, 답변하기 
-//참가 신청 & 반응하기
+//Q&A 등록, 답변하기, 수정하기 
+
+//참가 신청
+
+//반응하기
 //참가 신청 - 알람에서 다이얼로그로 확인 -> 게시글 제목, 프로필, 메시지, 승인 or 거절
 //알림 페이지 구현 - 알람 테이블 에서 얻어오기 알람 읽기 -> 자세히 보기, 삭제 기능 넣기 
 
@@ -18,7 +28,8 @@ import RegisterProfile from './RegisterProfileComponent';
 import EditProfile from './EditProfileComponent';
 import CreatePostComponent from './CreatePostComponent';
 import PostDetailComponent from './PostDetailComponent';
-
+import PostEditComponent from './EditPostComponent';
+import NotificationComponent from './NotificationComponent';
 
 function App() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
@@ -33,6 +44,8 @@ function App() {
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/create-post" element={<CreatePostComponent />} />
         <Route path="/post/:postId" element={<PostDetailComponent />} />
+        <Route path="/edit-post/:postId" element={<PostEditComponent />} />
+        <Route path="/notification" element={<NotificationComponent />} />
         <Route path="/" element={isAuthenticated ? <MainPageComponent /> : <LoginComponent />} />
       </Routes>
     </Router>
