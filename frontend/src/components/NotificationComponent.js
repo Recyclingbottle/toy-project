e304a77d4d09a39481a92b68644e5877815db05a
Recyclingbottle <hoholeo382@gmail.com>
2023-10-20 15:30:29 +0900

    질문 알림 -> 게시글로 이동, 참가요청 -> 참가 수락 및 거절 모달 추가

--- frontend/src/components/NotificationComponent.js
+++ frontend/src/components/NotificationComponent.js
@@ -5,11 +5,16 @@ import { Link } from 'react-router-dom';
 
 import Navbar from './NavbarComponent';
 import HeaderComponent from './HeaderComponent';
+import ProjectModal from './ProjectModal';
 
 function NotificationComponent() {
     const navigate = useNavigate();
     const token = useSelector(state => state.auth.token);
     const [notifications, setNotifications] = useState([]);
+    const [selectedNotification, setSelectedNotification] = useState(null); // State to store selected notification
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedProjectId, setSelectedProjectId] = useState(null); // 선택된 프로젝트의 ID
+
 
     useEffect(() => {
         const fetchNotificationData = async () => {
@@ -22,7 +27,7 @@ function NotificationComponent() {
                 });
                 const data = await response.json();
                 if (response.status === 200) {
-                    setNotifications(data); 
+                    setNotifications(data);
                 } else {
                     alert('알림을 가져오는 중 오류가 발생했습니다.');
                     navigate(-1);
@@ -45,10 +50,7 @@ function NotificationComponent() {
                 }
             });
             if (response.status === 200) {
-                // 알림 삭제가 성공하면 화면에서도 해당 알림을 삭제
                 setNotifications(notifications.filter(notification => notification.notification_id !== notificationId));
-                // 알림 삭제 후 알림 페이지로 리다이렉트
-                navigate('/notification');
             } else {
                 alert('알림을 삭제하는 중 오류가 발생했습니다.');
             }
@@ -56,25 +58,45 @@ function NotificationComponent() {
             alert('알림을 삭제하는 중 오류가 발생했습니다.');
         }
     };
+
+    const handleViewNotification = (notification) => {
+        setSelectedNotification(notification);
+    
+        if (notification.notification_type === '답변알림' || notification.notification_type === '질문요청') {
+            navigate(`/post/${notification.related_item_id}`);
+        } else if (notification.notification_type === '프로젝트참가요청') {
+            setSelectedProjectId(notification.related_item_id);
+            setIsModalOpen(true);
+        } else if (notification.notification_type === '참가수락' || notification.notification_type === '참가거절') {
+            alert('TEST'); 
+        }
+    };
+    
+    const handleCloseModal = () => {
+        // 모달 닫기
+        setIsModalOpen(false);
+    };
     return (
-  <div>
-    <Navbar />
-    <HeaderComponent />
-    <h1>알림 페이지</h1>
-    <ul>
-      {notifications.map(notification => (
-        <li key={notification.notification_id}>
-          <strong>메시지:</strong> {notification.notification_message}<br />
-          <strong>유형:</strong> {notification.notification_type}<br />
-          <button onClick={() => handleDeleteNotification(notification.notification_id)}>삭제</button>
-          <Link to={`/post/${notification.related_item_id}`}>
-            <button>확인</button>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+        <div>
+            <Navbar />
+            <HeaderComponent />
+            <h1>알림 페이지</h1>
+            <ul>
+                {notifications.map(notification => (
+                    <li key={notification.notification_id}>
+                        <strong>메시지:</strong> {notification.notification_message}<br />
+                        <strong>유형:</strong> {notification.notification_type}<br />
+                        <button onClick={() => handleDeleteNotification(notification.notification_id)}>삭제</button>
+                        <button onClick={() => handleViewNotification(notification)}>확인</button>
+                    </li>
+                ))}
+            </ul>
+            {isModalOpen && (
+    <ProjectModal projectId={selectedProjectId} onClose={handleCloseModal} />
+    )}
+        </div>
+    );
+
 }
 
 export default NotificationComponent;
